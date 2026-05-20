92305892edad7ba47f9fa7948f3a1771d349a2d8
Lorenzo Celiento <lorenzo.celiento@doctums.com>
2026-05-20 12:46:55 -0500

    Update site - Firestore PBs, races, pictures folder, clean file structure

---
+++ runners.js
@@ -0,0 +1,17 @@
+// Add runner names here to show them on the homepage.
+// Make sure a matching photo file exists in the folder (e.g. Lorenzo.png -> "Lorenzo").
+window.RUNNERS = [
+  "Eric",
+  "Will",
+  "Stu",
+  "Ignacio",
+  "Kyle",
+  "Tom",
+  "Christian",
+  "Stan",
+  "Clayton",
+  "Karl",
+  "Mark",
+  "Mike",
+  "Lorenzo"
+];
